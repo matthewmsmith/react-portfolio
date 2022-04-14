fd10e49932e5d061971d58c1350fe187352c4fde
Matthew Smith <matthewsmith@Matthews-MacBook-Pro.local>
2022-04-14 06:42:27 -0400

    feat: add sections, icons, and profile picture

---
+++ src/components/header/HeaderSocial.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BsInstagram} from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs'
+const HeaderSocial = () => {
+  return (
+    <div className='header__socials'>
+      <a href='https://www.linkedin.com/in/matthewmsmith1992/' target='_blank'><BsLinkedin/></a>
+      <a href="https:instagram.com/motomattsmith" target='_blank'>
+        <BsInstagram />
+      </a>
+      
+    </div>
+  )
+}
+
+export default HeaderSocial
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const portfolio = () => {
+  return <div>portfolio</div>;
+};
+export default portfolio;
